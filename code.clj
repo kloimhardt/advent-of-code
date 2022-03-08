1b103608651c1954a39177c1f45483661d20711d
kloimhardt <markus.agwin@gmx.net>
2022-03-09 00:21:30 +0100

    some changes

---
+++ code.clj
@@ -0,0 +1,200 @@
+;; https://adventofcode.com/2021
+;; login with my github account
+
+(ns advent-of-code
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [clojure.set :as set]))
+
+(defn readfile [name]
+  (with-open [rdr (io/reader (io/file name))]
+    (doall (line-seq rdr))))
+
+;;day 1/1
+
+(defn day01-1 [data]
+  (->> (map - data (next data))
+       (filter neg?)
+       (count)))
+
+;;day 1/2
+
+(defn day01-2 [data]
+  (let [windows (map + data (next data) (nnext data))]
+    (->> (map - windows (next windows))
+         (filter neg?)
+         (count))))
+
+(comment
+
+  (def data (mapv parse-long (readfile "inputs/year2021/day1")))
+  (day01-1 data) ;; => 1316
+  (day01-2 data) ;; => 1344
+
+  )
+
+(comment
+
+  (parse-long "1")
+  (def data (mapv parse-long ["1" "-2" "3" "-4" "5"]))
+  (next data)
+  (nnext data)
+  (filter neg? data)
+  (->> data
+       (filter neg?))
+  (map + data data)
+  (count data)
+
+  )
+
+;;day 2/1
+
+(defn convert-text-into-keyword-and-number [text]
+  (let [[_ cmd arg] (re-matches #"([a-z]+) ([0-9]+)" text)]
+    [(keyword cmd) (parse-long arg)]))
+
+(defn day02-1 [data]
+  (let [[x y] (reduce
+                (fn [[x y] [cmd arg]]
+                  (case cmd
+                    :forward [(+ x arg) y]
+                    :down    [x (+ y arg)]
+                    :up      [x (- y arg)]))
+                [0 0] data)]
+    (* x y)))
+
+;; day 2/2
+
+(defn day02-2 [data]
+  (let [[x y _] (reduce
+                  (fn [[x y aim] [cmd arg]]
+                    (case cmd
+                      :forward [(+ x arg) (+ y (* aim arg)) aim]
+                      :down    [x y (+ aim arg)]
+                      :up      [x y (- aim arg)]))
+                  [0 0 0] data)]
+    (* x y)))
+
+(comment
+
+  (def data (->> "inputs/year2021/day2"
+                 readfile
+                 (mapv convert-text-into-keyword-and-number)))
+  (day02-1 data) ;; => 1893605
+  (day02-2 data) ;; => 2120734350
+
+  )
+
+(comment
+
+  (def data ["forward 4" "down 9" "forward 2" "forward 2" "down 7"])
+  (re-matches #"([a-z]+) ([0-9]+)" "forward 4")
+  (keyword "forward")
+
+  )
+
+;;day 3/1
+
+(defn transpose [m]
+  (apply map vector m))
+
+(defn digits [lines]
+  (->> lines
+       transpose
+       (map frequencies)
+       (map (fn [{zeros \0 ones \1 :or {zeros 0 ones 0}}]
+              (if (>= ones zeros) 1 0)))))
+
+(defn day03-1 [lines]
+  (let [digits (digits lines)
+        invert-digits (mapv #(- 1 %) digits)
+        γ      (Long/parseLong (apply str digits) 2)
+        ε      (Long/parseLong (apply str invert-digits) 2)]
+    (* γ ε)))
+
+;;day 3/2
+
+(defn most-common [lines invert?]
+  (loop [filtered-lines lines
+         pos 0]
+    (if (or (<= (count filtered-lines) 1) (> pos (count (first lines))))
+      filtered-lines
+      (let [digit (nth (digits filtered-lines) pos)
+            char-digit (-> (if invert? (- 1 digit) digit) str first)]
+        (recur (filter #(= (nth % pos) char-digit) filtered-lines)
+               (inc pos))))))
+
+(defn day03-2 [lines]
+  (let [O₂    (Long/parseLong (first (most-common lines false)) 2)
+        CO₂   (Long/parseLong (first (most-common lines true)) 2)]
+    (* O₂ CO₂)))
+
+(comment
+
+  (def lines (readfile "inputs/year2021/day3"))
+  (day03-1 lines) ;; => 4138664
+  (day03-2 lines) ;; => 4273224
+
+  )
+
+(comment
+
+  (def data "1001")
+  (frequencies data)
+  (transpose [[1 2] [3 4]])
+
+  )
+
+;;day 4 is perfect in day4.clj
+
+;;day 5/1
+
+(defn straight? [[x1 y1 x2 y2]] (or (= x1 x2) (= y1 y2)))
+
+(defn make-line [input-str]
+  (let [[_ x1 y1 x2 y2] (re-matches #"(\d+),(\d+) -> (\d+),(\d+)" input-str)]
+    (map parse-long [x1 y1 x2 y2])))
+
+(defn abs-range
+  [a b]
+  (cond
+    (< a b) (range a (inc b))
+    (> a b) (range a (dec b) -1)
+    (= a b) (repeat a)))
+
+(defn points
+  [[x1 y1 x2 y2]]
+  (map vector (abs-range x1 x2) (abs-range y1 y2)))
+
+
+(defn day05-1 [input]
+  (->> input
+       (map make-line)
+       (filter straight?)
+       (map points)
+       (apply concat)
+       frequencies
+       (filter (fn [[_point num-of-intersections]]
+                 (> num-of-intersections 1)))
+       count))
+
+;;day 5/2
+
+(defn day05-2 [input]
+  (->> input
+       (map make-line)
+       (map points)
+       (apply concat)
+       frequencies
+       (filter (fn [[_point num-of-intersections]]
+                 (> num-of-intersections 1)))
+       count))
+
+(comment
+
+  (def input (readfile "inputs/year2021/day5"))
+  (day05-1 input) ;; => 5698
+  (day05-2 input) ;; => 15463
+
+  )
--- src/advent_of_code/year2021/day5.clj
+++ src/advent_of_code/year2021/day5.clj
@@ -4,21 +4,22 @@
    [clojure.java.math :as math]
    [clojure.string :as str]
    [clojure.set :as set]
+   [clojure.string :as string]
    #_[clj-async-profiler.core :as profiler]))
 
 (defrecord Vent [x1 y1 x2 y2])
 
 (defn parse-vent [s]
   (let [[x1 y1 x2 y2] (->> (re-matches #"\s*(\d+)\s*,\s*(\d+)\s*->\s*(\d+)\s*,\s*(\d+)\s*" s)
-                        (next)
-                        (mapv parse-long))]
+                           (next)
+                           (mapv parse-long))]
     (Vent. x1 y1 x2 y2)))
 
 (defn parse [input]
   (-> input
-    (str/trim)
-    (str/split #"\n")
-    (->> (mapv parse-vent))))
+      (str/trim)
+      (str/split #"\n")
+      (->> (mapv parse-vent))))
 
 (def example
   (slurp "inputs/year2021/day5.example"))
@@ -56,13 +57,13 @@
 (defn part1
   ([] (part1 input))
   ([input]
-    (solve (->> (parse input)
-             (filter #(or (= (:x1 %) (:x2 %)) (= (:y1 %) (:y2 %))))))))
+   (solve (->> (parse input)
+               (filter #(or (= (:x1 %) (:x2 %)) (= (:y1 %) (:y2 %))))))))
 
 (defn part2
   ([] (part2 input))
   ([input]
-    (solve (parse input))))
+   (solve (parse input))))
 
 (comment
   (do
@@ -73,3 +74,74 @@
   (profiler/profile (part1))
   (part2 example)
   (part2))
+
+;;--------- solution https://github.com/neilcode/advent-of-code-2021/blob/main/src/calendar/2021/day_05.clj
+
+(defn straight? [{{x' :x y' :y} :source {x :x y :y} :dest :as line}] (or (= x' x) (= y' y)))
+
+(defn make-line [input-str]
+  (let [[_ x1 y1 x2 y2] (re-matches #"(\d+),(\d+) -> (\d+),(\d+)" input-str)]
+    {:source {:x (Integer/parseInt x1) :y (Integer/parseInt y1)}
+     :dest   {:x (Integer/parseInt x2) :y (Integer/parseInt y2)}}))
+
+(defn abs-range
+  "Generates a range between two points on a line regardless of the order in which the points are given"
+  [a b]
+  (cond
+    (< a b) (range a (inc b))    ;; incrementing range
+    (> a b) (range a (dec b) -1) ;; decrementing range
+    (= a b) (repeat a)))         ;; infinite range :D
+
+(defn points
+  "Returns a collection of [x y] coordinates that comprise a given line"
+  [{{x' :x y' :y} :source {x :x y :y} :dest :as line}]
+  (map vector (abs-range x' x) (abs-range y' y)))
+
+(comment
+  (points {:source {:x 692, :y 826}, :dest {:x 692, :y 915}}))
+
+(defn xy->intersections
+  ([line] (xy->intersections line {}))
+  ([line graph]
+   (reduce (fn [graph point] (update graph point #(inc (or % 0))))
+           graph
+           (points line))))
+
+(defn graph-lines
+  ([lines] (graph-lines {} lines))
+  ([graph lines] (reduce #(xy->intersections %2 %1) graph lines)))
+
+(defn part-one
+  [input]
+  (->> (map make-line input)
+       (filter straight?)
+       graph-lines
+       (filter (fn [[_point num-of-intersections]]
+                 (> num-of-intersections 1)))
+       count))
+
+(defn part-two
+  [input]
+  (->> (map make-line input)
+       graph-lines
+       (filter (fn [[_point num-of-intersections]]
+                 (> num-of-intersections 1)))
+       count))
+
+(defn each-line
+  "Returns a list of strings representing lines in the given file"
+  [filename & {:keys [root path matcher mapfn]
+               :or
+               {root (System/getProperty "user.dir")
+                path "resources/"
+                matcher #"\n"
+                mapfn identity}}]
+  (as-> (str root "/" path filename) $input
+    (slurp $input)
+    (string/split $input matcher)
+    (map mapfn $input)))
+
+(def input-new (each-line "day5" :path "inputs/year2021/"))
+
+(= (part-one input-new) 5698)
+(= (part-two input-new) 15463)
